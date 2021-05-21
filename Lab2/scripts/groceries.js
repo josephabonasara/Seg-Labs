4f1dd9a27977ac6a2e3532ffcacf024625b47825
Joseph Abonasara <abojos23@gmail.com>
2021-05-21 15:55:31 -0400

    push

--- Lab2/scripts/groceries.js
+++ Lab2/scripts/groceries.js
@@ -7,20 +7,72 @@ var products = [
 		name: "brocoli",
 		lactoseFree: true,
 		nutFree: true,
+		organic: true,
 		price: 1.99
 	},
 	{
 		name: "bread",
 		lactoseFree: true,
 		nutFree: false,
+		organic: true,
 		price: 2.35
 	},
 	{
 		name: "salmon",
 		lactoseFree: false,
 		nutFree: true,
+		organic: true,
 		price: 10.00
-	}
+	},
+    {
+        name: "Dozen Eggs",
+        lactoseFree: false,
+		nutFree: false,
+		organic: true,
+        price: 2.50
+    },
+    {
+        name: "Apple",
+        lactoseFree: true,
+		nutFree: true,
+		organic: true,
+        price: 1.00
+    },
+    {
+        name: "Nutella",
+        lactoseFree: true,
+		nutFree: false,
+		organic: true,
+        price: 5.00
+    },
+    {
+        name: "nutFree Spinach Pack",
+        lactoseFree: true,
+		nutFree: true,
+		organic: true,
+        price: 5.00
+    },
+    {
+        name: "Frosted Flakes",
+        lactoseFree: true,
+		nutFree: false,
+		organic: true,
+        price: 4.00
+    },
+    {
+        name: "Ground Beef",
+        lactoseFree: false,
+		nutFree: false,
+		organic: true,
+        price: 10.00
+    },
+    {
+        name: "Chicken Breast",
+        lactoseFree: false,
+		nutFree: false,
+		organic: true,
+        price: 10.00
+    }
 ];
 	
 
@@ -31,20 +83,34 @@ var products = [
 function restrictListProducts(prods, restriction) {
 	let product_names = [];
 	for (let i=0; i<prods.length; i+=1) {
-		if ((restriction == "Lactose-intolerant") && (prods[i].lactoseFree == true)){
-			product_names.push(prods[i].name);
+		if (restriction=="None"){
+			product_names.push([prods[i].name, prods[i].price]);	
+        }
+		else if ((restriction == "Lactose-intolerant Nutallergy and Organic") && (prods[i].lactoseFree == true && (prods[i].nutFree == true)&& (prods[i].organic== true))){
+			product_names.push([prods[i].name, prods[i].price]);
 		}
-		else if ((restriction == "Nutallergy") && (prods[i].nutFree == true)){
-			product_names.push(prods[i].name);
+		else if ((restriction == "Lactose-intolerant and Nutallergy") && (prods[i].lactoseFree == true && (prods[i].nutFree == true))){
+			product_names.push([prods[i].name, prods[i].price]);
 		}
-		else if (restriction == "None"){
-			product_names.push(prods[i].name);
+		else if ((restriction == "Lactose-intolerant and Organic") && (prods[i].lactoseFree == true && (prods[i].organic == true))){
+			product_names.push([prods[i].name, prods[i].price]);
+		}
+		else if ((restriction == "Nutallergy and Organic") && (prods[i].organic == true && (prods[i].nutFree == true))){
+			product_names.push([prods[i].name, prods[i].price]);
+		}
+		else if ((restriction == "lactoseFree") && (prods[i].lactoseFree == true)){
+			product_names.push([prods[i].name, prods[i].price]);
+		}
+		else if ((restriction == "nutFree") && (prods[i].nutFree == true)){
+			product_names.push([prods[i].name, prods[i].price]);
+		}
+		else if (restriction == "organic" && prods[i].organic==true){
+			product_names.push([prods[i].name, prods[i].price]);	
 		}
 	}
-	product_names.sort(function(a, b) {
-		return parseFloat(a.price) - parseFloat(b.price);
-	});
-	return product_names;
+	return product_names.sort(function([a,b], [c,d]){
+		return b-d;
+	});;
 }
 
 // Calculate the total price of items, with received parameter being a list of products
