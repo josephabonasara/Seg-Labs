40bb48d3faa2d9ab1377f167571c13b3b4c21464
Joseph Abonasara <abojos23@gmail.com>
2021-06-10 20:04:18 -0400

    updates

---
+++ Lab5/script/service.js
@@ -0,0 +1,24 @@
+function validatePhone(txtPhone) {
+    var a = document.getElementById(txtPhone).value;
+    
+    var filter = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+    if (filter.test(a)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function validateHelper(type){
+    if (type=="phone"){
+        if (!validatePhone("phone")){
+            alert("Wrong format for phone");
+            $("#phone").val("(xxxx)");
+            $("#phone").addClass("error");
+        }
+        else {
+            $("#phone").removeClass("error");
+        }
+    }   
+}
