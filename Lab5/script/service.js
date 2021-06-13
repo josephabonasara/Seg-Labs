8a3cce8917c2f2c08d045c4c6986dbc106cf48a4
Joseph Abonasara <abojos23@gmail.com>
2021-06-13 16:36:06 -0400

    Changed how to make appointment, lots of changes left tho

--- Lab5/script/service.js
+++ Lab5/script/service.js
@@ -1,24 +1,52 @@
-function validatePhone(txtPhone) {
-    var a = document.getElementById(txtPhone).value;
-    
-    var filter = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
-    if (filter.test(a)) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
 
-function validateHelper(type){
-    if (type=="phone"){
-        if (!validatePhone("phone")){
-            alert("Wrong format for phone");
-            $("#phone").val("(xxxx)");
-            $("#phone").addClass("error");
-        }
-        else {
-            $("#phone").removeClass("error");
-        }
-    }   
-}
+let filter;
+
+var times = {
+    "Any": anytime [6,0],
+    "Giorgio Frigo": [5,1,2,3],
+    "Julie Lope": [0,2,4,6],
+    "Megan Kole": [1,2,3,4]
+};
+
+
+
+
+$(document).ready(function(){
+
+    var expert = $("#expert");
+    var calendar = $("#datetime");
+    date();
+
+    $("#datetime").datetime({
+        disabledWeekDays: filter
+    });
+    
+    $('#toggle').on("click", function(){
+        $('#datetime').datetime("show");
+    });
+
+    $("#expert").on("change", function(){
+        
+        $('#datetime').datetime("destroy");
+        date();
+        var container = document.getElementById("holdCalendar");
+        var content = container.innerHTML;
+        container.innerHTML= " "; 
+        container.innerHTML= content; 
+        $("#datetime").datetime({
+            disabledWeekDays: filter
+        });
+        $('#toggle').on("click", function(){
+            $('#datetime').datetime("show");
+        });  
+    });
+
+    
+    function date(){
+        filter = times[ expert.children("option:selected").text()];
+        return filter;
+    };
+
+
+
+});
